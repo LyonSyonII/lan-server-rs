1d012574a9eec7df189ac0d373edfb640b3c9110
lyonsyonii <liam@garriga.dev>
2023-12-22 10:36:24 +0100

    Axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use axum::routing::get;
+
+
+#[tokio::main]
+async fn main() {
+    let ip = local_ip_address::local_ip().expect("Could not get local IP address!");
+    let addr = std::net::SocketAddr::new(ip, 5555);
+
+    let app = axum::Router::new().route("/", get(|| async { "Hello, World!" }));
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap_or_else(|_| { panic!("Could not bind to address: {addr}") });
+    println!("Listening on http://{addr}");
+    axum::serve(listener, app).await.expect("Could not start server!");
 }

073cf92faa5dba677d634f580215aa9acd94b58c
lyonsyonii <liam@garriga.dev>
2023-12-22 11:49:11 +0100

    actix-web

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,20 @@ async fn hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    #[cfg(debug_assertions)]
+    {
+        env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    }
 
     let ip = local_ip_address::local_ip().unwrap();
-    actix_web::HttpServer::new(|| {
+    let server = actix_web::HttpServer::new(|| {
         actix_web::App::new()
             .wrap(actix_web::middleware::Logger::default())
             .service(hello)
     })
     .bind((ip, 5555))?
-    .run()
-    .await
+    .run();
+
+    println!("Listening on http://{}:5555", ip);
+    server.await
 }

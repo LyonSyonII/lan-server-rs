d8cb04d477f401b3e65e3795d9fb0cbb7f0bb52c
lyonsyonii <liam@garriga.dev>
2024-04-07 17:49:49 +0200

    layout changes

--- hitori-webapp/src/components/IndexElement.ts
+++ hitori-webapp/src/components/IndexElement.ts
@@ -2,6 +2,25 @@ import type { ChannelListElement, Channel } from "../components/channel-list/Cha
 import { MessageListElement, type Message } from "../components/message-list/MessageListElement";
 import DbUrl from "/DB.json?url"
 
+export async function refreshState() {
+  type Database = {
+    messages: Message[][];
+    channels: Channel[];
+  };
+  const database: Database = await fetch(DbUrl).then(r => r.json());
+  
+  const channelList: ChannelListElement = document.body.querySelector("x-channel-list")!;
+  channelList.addChannels(database.channels);
+  
+  const messageList: MessageListElement = document.body.querySelector("x-message-list")!;
+  await messageList.setMessages(database.messages[0]);
+  
+  channelList.clearOnSelect();
+  channelList.addOnSelect((i) => {
+    messageList.setMessages(database.messages[i])
+ });
+}
+
 export class IndexElement extends HTMLElement {
   public constructor() {
     super();
